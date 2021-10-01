efbd7672122da06e4481516d7c57e9e1eddc96c3
hxchua00 <hxchua00@gmail.com>
2021-10-01 15:18:02 +0800

    Add project files.

---
+++ ThreadedList/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ThreadedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool cont = true;
+            while (cont)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Activating List.....");
+                Console.WriteLine("Enter your choice: ");
+                Console.WriteLine();
+                Console.WriteLine("1) Add name to list");
+                Console.WriteLine("2) Remove name to list");
+                Console.WriteLine("3) Exit");
+                Console.WriteLine();
+                int choice = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter name to add: ");
+                        string aName = Console.ReadLine();
+                        Console.WriteLine();
+
+                        Thread a = new Thread(()=> { ToList.Add(aName); });
+                        a.Start();
+                        a.Join();
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter name to remove: ");
+                        string rName = Console.ReadLine();
+                        Console.WriteLine();
+
+                        Thread r = new Thread(() => { ToList.Remove(rName); });
+                        r.Start();
+                        r.Join();
+                        break;
+                    default:
+                        Console.WriteLine("Good bye!");
+                        Console.WriteLine();
+                        cont = false;
+                        break;
+                }
+            }
+            
+        }
+
+        class ToList
+        {
+            static List<string> nList = new List<string>();
+
+            public static void Add(string str)
+            {
+                nList.Add(str);
+                Console.WriteLine("{0} has been added to the list!",str);
+                Console.WriteLine();
+
+                Console.WriteLine("Names in the list now: ");
+                foreach (string s in nList)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+
+            public static void Remove(string str)
+            {
+                if (nList.Contains(str))
+                {
+                    nList.Remove(str);
+                    Console.WriteLine("{0} has been removed from the list!",str);
+                    Console.WriteLine();
+
+                    Console.WriteLine("Names in the list now: ");
+                    foreach (string s in nList)
+                    {
+                        Console.WriteLine(s);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Sorry! {0} cannot be found in the list!",str);
+                    Console.WriteLine("Please enter a new name!");
+                    Console.WriteLine();
+
+                    Console.WriteLine("Names in the list now: ");
+                    foreach (string s in nList)
+                    {
+                        Console.WriteLine(s);
+                    }
+                }
+                
+            }
+        }
+    }
+}
---
+++ ThreadedList/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ThreadedList")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ThreadedList")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cd2bae24-2a34-4b68-ab82-9dda63b49631")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
